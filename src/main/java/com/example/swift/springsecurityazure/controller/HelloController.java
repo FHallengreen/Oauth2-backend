d677e303e7b1f49fa14f007567760f18c18bc570
FHallengreen <fred70p9@stud.kea.dk>
2023-05-16 15:57:23 +0200

    changes

--- src/main/java/com/example/swift/springsecurityazure/controller/HelloController.java
+++ src/main/java/com/example/swift/springsecurityazure/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-
+@CrossOrigin
 public class HelloController {
 
     @GetMapping("/")
